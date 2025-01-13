4fafa74e822eefef07d8000b2af5b0c792dbf1df
Ali nasir <alinasirakram1@gmail.com>
2025-01-13 14:23:36 +0500

    stars added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from data_processing import load_data
 
 app = Flask(__name__)
 
@@ -6,6 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+    print(load_data())
 
 
 if __name__ == '__main__':
